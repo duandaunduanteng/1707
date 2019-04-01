d6b1e36d75c54c22f98c6c921a5a30c9707df8d8
zhangdada521 <zhangdada521@qq.com>
2019-04-01 10:31:17 +0800

    解决中文乱码

--- Git1707Pro/src/com/group1707/test/Test1.java
+++ Git1707Pro/src/com/group1707/test/Test1.java
@@ -3,7 +3,7 @@ package com.group1707.test;
 public class Test1 {
 
 	public static void main(String[] args) {
-			System.out.println("²âÊÔgit¹ÜÀí´úÂë");
-			System.out.println("À²À²À²");
+			System.out.println("ä¸­æ–‡");
+			System.out.println("è§£å†³ä¸­æ–‡");
 	}
 }
