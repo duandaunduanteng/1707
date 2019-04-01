4d612df27dc1f1c89f8296088b8b79cd5a9fe58f
duandaunduanteng <526177188@>
2019-04-01 16:34:17 +0800

    老段修改

--- dwww/src/dwww/BingDiTest.java
+++ dwww/src/dwww/BingDiTest.java
@@ -4,6 +4,7 @@ public class BingDiTest {
 
 	public static void main(String[] args) {
 		System.out.println("我是冰帝！");
+		System.out.println("我是文龙");
 	}
 
 }
--- dwww/src/dwww/Test.java
+++ dwww/src/dwww/Test.java
@@ -3,5 +3,6 @@ package dwww;
 public class Test {
 	public static void main(String[] args) {
 	System.out.println("123");
+	System.out.println("Ïþ·å¹Û¿´");
 }
 }
